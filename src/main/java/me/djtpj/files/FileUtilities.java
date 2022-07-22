f5e39f2c2a46c3f3e9043e177bb6f0ea03aa34d4
Djtpj <93952263+Djtpj@users.noreply.github.com>
2022-07-21 22:42:40 -0500

    FileUtilities.java updated

--- src/main/java/me/djtpj/files/FileUtilities.java
+++ src/main/java/me/djtpj/files/FileUtilities.java
@@ -5,11 +5,22 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/** Utility of things that should be able to be done quickly in Java, but cannot be done easily or quickly.
+ *
+ */
 public class FileUtilities {
+
+    private static BufferedReader bufferedReader;
+
+    /** Reads all the text from the given file
+     * @param file the file to read
+     * @return all the text that was read
+     */
     public static String readTextFromFile(File file) {
         StringBuilder results = new StringBuilder();
 
@@ -20,22 +31,27 @@ public class FileUtilities {
         return results.toString();
     }
 
+    /** Reads all the lines from the given file
+     * @param file the file to read
+     * @return the lines from the file
+     */
     public static String[] readTextFromFileAsLines(File file) {
-        ArrayList<String> lines = new ArrayList<>();
         try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
+            bufferedReader = new BufferedReader(new FileReader(file));
 
-            while (br.readLine() != null) {
-                lines.add(br.readLine());
-            }
+            Stream<String> lines = bufferedReader.lines();
+
+            return lines.toArray(String[]::new);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return lines.toArray(new String[0]);
     }
 
+    /** Creates the file in the given location, but only creates if that file does not exist
+     * @param file the file to create (the path is read from the file)
+     */
     public static void makeFile(File file) {
         if (!file.exists()) {
             try {
@@ -50,6 +66,10 @@ public class FileUtilities {
         }
     }
 
+    /** Lists all the files in a given directory
+     * @param absPath the absolute path of the directory to read
+     * @return the files in the given directory
+     */
     public static File[] listAllFiles(String absPath) {
         Path start = Paths.get(absPath);
         ArrayList<File> files = new ArrayList<>();
@@ -71,6 +91,10 @@ public class FileUtilities {
         return files.toArray(new File[files.size()]);
     }
 
+    /**
+     * @param absPath the path to validate
+     * @return whether a path is valid or not
+     */
     public static boolean isValidPath(String absPath) {
         if (!absPath.contains("\\")) {
             return false;
@@ -92,13 +116,26 @@ public class FileUtilities {
         }
     }
 
-    public static void writeToFile(String absPath, String content) {
+    /** Writes the content string to the file in the given path
+     * @param content the text you want to write to a file
+     * @param absPath the absolute path of the file to write to
+     * @see FileUtilities#isValidPath(String)
+     */
+    public static void writeToFile(String content, String absPath) {
         try {
             FileWriter writer = new FileWriter(absPath);
             writer.write(content);
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void cleanUp() {
+        try {
+            bufferedReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
