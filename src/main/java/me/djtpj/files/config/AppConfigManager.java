0536f7bf462dd43ba9c597c8d636afb7b8c13777
Djtpj <93952263+Djtpj@users.noreply.github.com>
2022-07-23 15:17:21 -0500

    function in DefaultConfigManager apparently wasn't filled, fixed that

--- src/main/java/me/djtpj/files/config/AppConfigManager.java
+++ src/main/java/me/djtpj/files/config/AppConfigManager.java
@@ -2,19 +2,35 @@ package me.djtpj.files.config;
 
 import java.io.File;
 
+/**
+ * App Config Manager's control JSON Configurations in Ridge apps
+ */
 public interface AppConfigManager {
+
+    /**
+     * @param config the config to add
+     */
     AppConfigManager addConfig(Config config);
 
+    /**
+     * @param key the key of the configuration to remove
+     */
     AppConfigManager removeConfig(String key);
 
     Config[] getConfigs();
 
-    <T> T getConfig(String key);
+    <T extends Config> T getConfig(String key);
 
+    /** Writes the configurations to the specified path in JSON format
+     * @param path the path to write to
+     */
     AppConfigManager write(String path);
 
     AppConfigManager write();
 
+    /** Reads the configurations from the specified path as dictated
+     * @param path the path to read from
+     */
     AppConfigManager read(String path);
 
     AppConfigManager read(File file);
--- src/main/java/me/djtpj/files/config/_default/DefaultConfigManager.java
+++ src/main/java/me/djtpj/files/config/_default/DefaultConfigManager.java
@@ -11,6 +11,7 @@ import org.json.simple.parser.ParseException;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class DefaultConfigManager implements AppConfigManager {
 
@@ -19,11 +20,26 @@ public class DefaultConfigManager implements AppConfigManager {
 
     private final ArrayList<Config<?>> configs;
 
-    public DefaultConfigManager(String path) {
-        this.path = path;
+    /**
+     * @param configFile the path to the config.json file
+     */
+    public DefaultConfigManager(String configFile) {
+        this.path = configFile;
         this.configs = new ArrayList<>();
     }
 
+    /**
+     * @param configFile the config.json file
+     */
+    public DefaultConfigManager(File configFile) {
+        this.path = configFile.getAbsolutePath();
+
+        this.configs = new ArrayList<>();
+    }
+
+    /**
+     * @param config the config to add
+     */
     @Override
     public AppConfigManager addConfig(Config config) {
         configs.add(config);
@@ -42,8 +58,15 @@ public class DefaultConfigManager implements AppConfigManager {
     }
 
     @Override
-    public <T> T getConfig(String key) {
-        return null;
+    public <T extends Config> T getConfig(String key) {
+
+        ArrayList<Config<?>> configs = this.configs;
+
+        Predicate<Config<?>> filter = key::equals;
+
+        configs.removeIf(filter.negate());
+
+        return (T) configs.get(0);
     }
 
     @Override
