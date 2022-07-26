ff8b2e273b3c3ceca6f67147c56ee613de66b2a3
Djtpj <93952263+Djtpj@users.noreply.github.com>
2022-07-26 15:17:59 -0500

    deleted module-info.java

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module Ridge {
-    requires static lombok;
-    requires json.simple;
-
-    exports me.djtpj.files;
-    exports me.djtpj.files.config;
-    exports me.djtpj.files.config._default;
-    exports me.djtpj.logger;
-}
